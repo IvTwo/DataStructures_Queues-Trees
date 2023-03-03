81f900cad8c17ab5a5b47213b7dd1bfaa5c3c39b
ivu-u <123986386+IvTwo@users.noreply.github.com>
2023-03-02 20:38:10 -0500

    initial + setup

---
+++ QueueCannon/QueueCannon/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace QueueCannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numShells = 5;
+            Queue<string> ammunition = new Queue<string>();
+
+            // add numShells to the ammunition queue
+            for (int i = 0; i < numShells; i++)
+            {
+                ammunition.Enqueue("shell " + i);
+            }
+
+            // main game loop
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            while(keyInfo.Key != ConsoleKey.Escape)
+            {
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    // fire shell
+                }
+            }
+        }
+    }
+}
