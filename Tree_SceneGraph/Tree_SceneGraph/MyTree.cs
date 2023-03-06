cfee7fb28a816d92ede24bf636bfdd61aa527245
ivu-u <123986386+IvTwo@users.noreply.github.com>
2023-03-06 13:04:10 -0500

    adding elements to the tree

---
+++ Tree_SceneGraph/Tree_SceneGraph/MyTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+// Reference Used: https://stackoverflow.com/questions/66893/tree-data-structure-in-c-sharp
+
+namespace Tree_SceneGraph
+{
+    internal class MyTree<T>
+    {
+        private T data;
+        private LinkedList<MyTree<T>> children;
+
+        // constructor
+        public MyTree(T data)
+        {
+            this.data = data;
+            children = new LinkedList<MyTree<T>>();
+        }
+
+        public void AddChild(T data)
+        {
+            children.AddLast(new MyTree<T>(data));
+        }
+
+        public void RemoveChild(T data)
+        {
+            // code here
+        }
+
+        public void PrintTree()
+        {
+            Console.WriteLine(data);
+            foreach (MyTree<T> kid in this.children)
+            {
+                Console.WriteLine(kid.data);
+            }
+        }
+
+        public bool PlayerInsert(MyTree<T> node, T newData, T parentData)
+        {
+            Console.WriteLine("parent data: " + parentData);
+            Console.WriteLine("node data: " + node.data);
+
+            // if you find the parent node, add newData as a child
+            // then exit the method
+            if (node.data.Equals(parentData))
+            {
+                Console.WriteLine("adding " + newData + " as a child of " + node.data);
+
+                node.AddChild(newData);
+                return true;
+            }
+
+            // recursion
+            foreach (MyTree<T> kid in node.children)
+            {
+                Console.WriteLine("Kid data: " + kid.data);
+                // kid becomes the new node, and search resumes
+                return PlayerInsert(kid, newData, parentData);     
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tree_SceneGraph/Tree_SceneGraph/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Tree_SceneGraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // testing
+            MyTree<string> tree = new MyTree<string>("scene");  // create instance of tree
+            tree.PlayerInsert(tree, "child", "scene");
+            Console.WriteLine();
+            tree.PlayerInsert(tree, "child2", "child");
+
+            Console.WriteLine();
+            tree.PrintTree();
+
+            //MyMenu();
+        }
+
+        // accept user input as a string
+        public static void MyMenu()
+        {
+            MyTree<string> tree = new MyTree<string>("scene");  // create instance of tree
+
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Welcome To The Scene Graph Simulator----!");
+                Console.WriteLine();
+                Console.WriteLine("The current tree looks like:");
+                // display the current tree
+                Console.WriteLine();
+                Console.WriteLine("\t1. Enter a Command\n" + "\t2. Quit\n");
+
+                switch (UserInput())    // handle user input
+                {
+                    case "1":
+                        
+                        break;
+
+                    case "2":
+                        keepGoing = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("---");
+                        Console.WriteLine("Invalid Input!");
+                        break;
+                }
+
+            }
+        }
+
+        // return the user input |  O(1)
+        public static string UserInput()
+        {
+            Console.Write("\t * Your Input: ");
+            return Console.ReadLine();
+        }
+    }
+}
