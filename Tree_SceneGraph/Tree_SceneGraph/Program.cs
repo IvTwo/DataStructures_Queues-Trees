a1cfeacfdbd3ad8d21359c2f52839de394a564db
ivu-u <123986386+IvTwo@users.noreply.github.com>
2023-03-08 13:05:56 -0500

    safe save

--- Tree_SceneGraph/Tree_SceneGraph/Program.cs
+++ Tree_SceneGraph/Tree_SceneGraph/Program.cs
@@ -4,28 +4,28 @@
     {
         static void Main(string[] args)
         {
-            // testing
-            MyTree<string> tree = new MyTree<string>("scene");  // create instance of tree
-            tree.PlayerInsert(tree, "child", "scene");
-            tree.PlayerInsert(tree, "child2", "child");
-            tree.PlayerInsert(tree, "child3", "child");
-            tree.PlayerInsert(tree, "child4", "scene");
-            Console.WriteLine();
-            bool test = tree.PlayerInsert(tree, "child5", "child3");
-            Console.WriteLine(test);
-            tree.PlayerInsert(tree, "child6", "child5");
+            // TESTING ------------
+            //MyTree<string> tree = new MyTree<string>("scene");  // create instance of tree
+            //tree.PlayerInsert(tree, "child", "scene");
+            //tree.PlayerInsert(tree, "child2", "child");
+            //tree.PlayerInsert(tree, "child3", "child");
+            //tree.PlayerInsert(tree, "child4", "scene");
+            //Console.WriteLine();
+            //bool test = tree.PlayerInsert(tree, "child5", "child3");
+            //Console.WriteLine(test);
+            //tree.PlayerInsert(tree, "child6", "child5");
 
-            Console.WriteLine();
-            tree.PrintTree(tree, 0);
-            Console.WriteLine();
+            //Console.WriteLine();
+            //tree.PrintTree(tree, 0);
+            //Console.WriteLine();
 
-            bool test2 = tree.PlayerDelete(tree, tree, "child5");
-            Console.WriteLine(test2);
-            Console.WriteLine();
-            tree.PrintTree(tree, 0);
+            //bool test2 = tree.PlayerDelete(tree, tree, "child5");
+            //Console.WriteLine(test2);
+            //Console.WriteLine();
+            //tree.PrintTree(tree, 0);
 
-
-            //MyMenu();
+            // PROGRAM START ------
+            MyMenu();
         }
 
         // accept user input as a string
@@ -38,20 +38,28 @@
             while (keepGoing)
             {
                 Console.WriteLine();
+                Console.WriteLine();
                 Console.WriteLine("Welcome To The Scene Graph Simulator----!");
                 Console.WriteLine();
-                Console.WriteLine("The current tree looks like:");
-                // display the current tree
-                Console.WriteLine();
-                Console.WriteLine("\t1. Enter a Command\n" + "\t2. Quit\n");
+                Console.WriteLine("\t1. Insert Node\n" + "\t2. Delete Node\n" +
+                                "\t3. Print Current Tree\n" + "\t4. Quit\n");
 
                 switch (UserInput())    // handle user input
                 {
                     case "1":
-                        
+                        UI_PlayerInsert(tree);
                         break;
 
                     case "2":
+                        break;
+
+                    case "3":
+                        Console.WriteLine("---");
+                        Console.WriteLine("Current Tree:");
+                        tree.PrintTree(tree, 0);    // print the tree
+                        break;
+
+                    case "4":
                         keepGoing = false;
                         break;
 
@@ -62,6 +70,34 @@
                 }
 
             }
+        }
+
+        // Text UI for the insert command
+        public static void UI_PlayerInsert(MyTree<string> tree)
+        {
+            Console.WriteLine("---");
+            Console.WriteLine("Please input your command as: \"insert A B\"");
+            Console.WriteLine("(A = child, B = parent)");
+            Console.WriteLine();
+
+            string userInput = UserInput();
+            string[] words = userInput.Split(' ');
+
+            // if the command doesn't start with "insert" or there are more than 3 words, exit the method
+            if (!words[0].Equals("insert") || words.Length > 3)
+            {
+                Console.WriteLine("---");
+                Console.WriteLine("Invalid Input!");
+                return;
+            }
+
+            bool didInsert = tree.PlayerInsert(tree, words[1], words[2]);
+            if(didInsert)
+            {
+                Console.WriteLine("---");
+                Console.WriteLine("Node Inserted!")
+            }
+
         }
 
         // return the user input |  O(1)
